2176ba4494adce771b4fe16f058debd7011390b8
riskidwiputra <riskidwiputra1250@gmail.com>
2022-10-25 06:10:06 +0700

    improve artikel

--- js/config.js
+++ js/config.js
@@ -1,7 +1,13 @@
 
 const users = 'https://634e2f9ff34e1ed82685122a.mockapi.io/users';
+const kategory = 'https://634e2f9ff34e1ed82685122a.mockapi.io/category'
+const articel = 'https://634e2f9ff34e1ed82685122a.mockapi.io/article';
 
 
 
 
-export default  users
+export default {
+    USER : users,
+    KATEGORY : kategory,
+    ARTIKEL : articel
+}
